5cb34f6ec18cd898042ad7293acd4ac39b5980c6
Shailja <shailja.cherished@gmail.com>
2020-03-08 23:51:37 +0900

    GradeBook
    Initial Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // double[] numbers = new double[] {12.7, 14.8, 15.9};
+            // var numbers = new[] {12.7, 14.8, 15.9, 4.1};
+
+            var grades = new List<double>() { 12.7, 14.8, 15.9, 4.1 };
+            var sum = 0.0;
+
+            foreach(double number in grades)
+            {
+                sum += number;
+            }
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+                Console.WriteLine(sum);
+                Console.WriteLine($"Average : {sum/grades.Count:N1}" );
+            }
+            else
+            {
+                System.Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
+ 
